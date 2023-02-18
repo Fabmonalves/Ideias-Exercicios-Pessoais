112968ae81280135feda4d64bd13c8528a67f632
Fabricio Monte <fab.alves46@gmail.com>
2023-02-18 03:06:55 -0300

    atualizações

---
+++ Ideias-programas/Tibia/calculadora_shared.py
@@ -0,0 +1,9 @@
+my_level = int(input("Seu level: "))
+shared_min = (my_level / 3) * 2
+shared_max = (my_level / 2) * 3
+
+
+
+print(f"Level minimo para sharear: \n{shared_min:.0f}" )
+print()
+print(f"Level maximo para sharear: \n{shared_max:.0f}" )
